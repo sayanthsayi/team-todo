3ac2eb63339fbde16e042af9900136d432c74b2b
sourav <souravt0077@gmail.com>
2023-07-22 12:00:28 +0530

    todo model created

--- user/userapp/urls.py
+++ user/userapp/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
-
     path('register/',views.UserRegister,name="UserRegister"),
     path('Login/',views.UserLogin,name="Login"),
     path('Logout/',views.UserLogout,name="Logout"),
