43667be0481b3ff76e7b874259cd88db6e360310
sourav <souravt0077@gmail.com>
2023-07-22 12:19:17 +0530

    read

--- user/todo/views.py
+++ user/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import TodoForm
 from django.contrib import messages
+from .models import TodoModel
 
 @login_required(login_url='Login')
 def home(request):
@@ -15,5 +16,7 @@ def home(request):
             user.save()
             messages.success(request,"Created")
             return redirect('home')
-    context={'form':form}
+    
+    tasks=TodoModel.objects.filter(user=request.user)
+    context={'form':form,'tasks':tasks}
     return render(request,'home.html',context)
