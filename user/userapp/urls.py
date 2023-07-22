c386b60dd52d26ff23fbb4877628437957b6802b
sourav <souravt0077@gmail.com>
2023-07-22 11:41:19 +0530

    initial commit

--- user/userapp/urls.py
+++ user/userapp/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 
-    path('home/',views.home,name='home'),
-    path('',views.UserRegister,name="UserRegister"),
+
+    path('register/',views.UserRegister,name="UserRegister"),
     path('Login/',views.UserLogin,name="Login"),
     path('Logout/',views.UserLogout,name="Logout"),
     path('confirmlogout/',views.ConfirmLogout,name="ConfirmLogout"),
