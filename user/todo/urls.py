736316a8b70e6c83b5e1496e03d667f7d4a0a3ad
sourav <souravt0077@gmail.com>
2023-07-22 12:31:28 +0530

    updated

--- user/todo/urls.py
+++ user/todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('update/<str:pk>/',views.update_task,name='update'),
 ]
--- user/todo/views.py
+++ user/todo/views.py
@@ -20,3 +20,18 @@ def home(request):
     tasks=TodoModel.objects.filter(user=request.user)
     context={'form':form,'tasks':tasks}
     return render(request,'home.html',context)
+
+def update_task(request,pk):
+    task=TodoModel.objects.get(id=pk)
+    form=TodoForm(instance=task)
+    if request.method == "POST":
+        form=TodoForm(request.POST,instance=task)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.user=request.user
+            user.task=user.task.capitalize()
+            user.save()
+            messages.success(request," {} Updated".format(task))
+            return redirect('home')
+    context={'form':form}
+    return render(request,'update.html',context)
