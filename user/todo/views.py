7993a0a74edf4c80e69896399a69b4a405bd3fe4
sourav <souravt0077@gmail.com>
2023-07-22 12:11:49 +0530

    crated

--- user/todo/views.py
+++ user/todo/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from .forms import TodoForm
+from django.contrib import messages
+
 @login_required(login_url='Login')
 def home(request):
-    return render(request,'home.html')
+    form=TodoForm
+    if request.method == 'POST':
+        form=TodoForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.user=request.user
+            user.task=user.task.capitalize()
+            user.save()
+            messages.success(request,"Created")
+            return redirect('home')
+    context={'form':form}
+    return render(request,'home.html',context)
